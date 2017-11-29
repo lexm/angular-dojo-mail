a60a2d2b10e881df275faa68b74f48f7558aa3cc
Lex Myers <lex.myers@gmail.com>
2017-11-29 14:28:00 -0800

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Dojo Mail';
+  emails = [
+    { email: 'Lex.Myers@GMAIL.com', important: true, subject: 'Alternative Fax', content: 'Here\'s some content' },
+    { email: 'FRED@SLATE.COM', important: false, subject: 'Mr. Slate', content: 'Hello???' },
+    { email: 'jarjarj@star.wars', important: true, subject: 'Me is bacca', content: 'See new movie, yassa>?' }
+  ];
 }
